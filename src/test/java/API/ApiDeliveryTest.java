23d2ced86a958a0cc93e10ac12bcec3e2285ba90
Olga <aleksandrova1olga@gmail.com>
2024-06-02 21:28:17 +0300

    Homework10

--- src/test/java/API/ApiDeliveryTest.java
+++ src/test/java/API/ApiDeliveryTest.java
@@ -1,7 +1,9 @@
 package API;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ApiDeliveryTest {
@@ -10,26 +12,27 @@ public class ApiDeliveryTest {
     public static final String BASE_PATH = "/test-orders/";
 
     @Test
-    public void checkOrderDetailsWithCorrectOrderId() {
-        RestAssured
-                .given()
-                .log()
-                .all()
-                .get(BASE_URL+BASE_PATH+"1")
-                .then()
-                .log()
-                .all()
-                .assertThat()
-                .statusCode(HttpStatus.SC_OK);
+    public void deleteAnExistingOrder(){
+        int orderIDRequested = 9;
+    RestAssured
+            .given()
+            .log()
+            .all()
+            .delete(BASE_URL+BASE_PATH+orderIDRequested)
+            .then()
+            .log()
+            .all()
+            .assertThat()
+            .statusCode(HttpStatus.SC_BAD_REQUEST);
     }
 
     @Test
-    public void checkErrorStatusCodeWithIncorrectOrderId() {
+    public void updateAnExistingOrderDetails(){
         RestAssured
                 .given()
                 .log()
                 .all()
-                .get(BASE_URL+BASE_PATH+"11")
+                .put(BASE_URL+BASE_PATH+"2")
                 .then()
                 .log()
                 .all()
@@ -38,16 +41,76 @@ public class ApiDeliveryTest {
     }
 
     @Test
-    public void checkErrorStatusCodeWithZeroOrderId() {
-        RestAssured
+    public void updateCustomerNameInExistingOrder(){
+        int orderIDRequested = 1;
+        String amendedOrderDetails = RestAssured
                 .given()
                 .log()
                 .all()
-                .get(BASE_URL+BASE_PATH+"0")
+                .queryParam("customerName")
+                .get(BASE_URL+BASE_PATH+orderIDRequested)
                 .then()
                 .log()
                 .all()
                 .assertThat()
-                .statusCode(HttpStatus.SC_BAD_REQUEST);
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .extract()
+                .path("customerName");
+
+        Assertions.assertEquals("Connor",amendedOrderDetails);
     }
-}
+
+    @Test
+    public void checkOrderAndResponse(){
+        int orderIDRequested = 5;
+        int recivedOrderId = RestAssured
+                .given()
+                .log()
+                .all()
+                .get(BASE_URL+BASE_PATH+orderIDRequested)
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .extract()
+                .path("id");
+
+        Assertions.assertEquals(orderIDRequested,recivedOrderId);
+    }
+
+    @Test
+    public void checkOrderCreationId(){
+        String requestBodyLongWay ="{\n" +
+                "  \"status\": \"OPEN\",\n" +
+                "  \"courierId\": 0,\n" +
+                "  \"customerName\": \"0\",\n" +
+                "  \"customerPhone\": \"0\",\n" +
+                "  \"comment\": \"0\",\n" +
+                "  \"id\": 0\n" +
+                "}";
+
+        long recivedOrderId = RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body(requestBodyLongWay)
+                .log()
+                .all()
+                .post(BASE_URL+BASE_PATH)
+                .then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .extract()
+                .path("id");
+
+  //      Assertions.assertEquals(recivedOrderId);
+    }
+    }
+
+
+
